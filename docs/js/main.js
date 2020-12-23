5e7de1cd9e27970286853eafb51a20c61db36855
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-12-23 23:21:47 +0200

    third commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,27 @@
-$(function(){
 
-});
+(function(){
+
+	const burger = document.querySelector('.button-burger');
+	const navigation = document.querySelector('.navigation');
+
+	burger.addEventListener('click', function(){
+		navigation.classList.toggle('active');
+		burger.classList.toggle('active');
+	})
+
+// ----------------------smooth scrolling-------------------------
+	const links = document.querySelectorAll('.link');
+
+	for(let i = 0; i < links.length; i++){
+		links[i].addEventListener('click', function(event){
+			event.preventDefault();
+			const blockId = event.target.getAttribute('href').substr(1);
+
+			document.getElementById(blockId).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		});
+	}
+
+}());
