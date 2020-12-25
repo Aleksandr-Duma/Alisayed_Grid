a1229bbd831b5f710b04a26d0ecc1cd653f51ed9
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-12-25 22:16:33 +0200

    fix menu-list

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,18 +3,19 @@
 
 	const burger = document.querySelector('.button-burger');
 	const navigation = document.querySelector('.navigation');
+	const links = document.querySelectorAll('.link');
 
 	burger.addEventListener('click', function(){
 		navigation.classList.toggle('active');
 		burger.classList.toggle('active');
-	})
-
-// ----------------------smooth scrolling-------------------------
-	const links = document.querySelectorAll('.link');
-
+	});
+	// smooth scrolling
 	for(let i = 0; i < links.length; i++){
 		links[i].addEventListener('click', function(event){
 			event.preventDefault();
+	// remove classes menu
+			burger.classList.remove('active');
+			navigation.classList.remove('active');
 			const blockId = event.target.getAttribute('href').substr(1);
 
 			document.getElementById(blockId).scrollIntoView({
